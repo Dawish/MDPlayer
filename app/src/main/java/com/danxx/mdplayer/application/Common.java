7edc5d080b206ed5239c41cee3847b92c1163d45
Dawish <742637254@qq.com>
2016-06-04 17:28:11 +0800

    # add img ur

--- app/src/main/java/com/danxx/mdplayer/application/Common.java
+++ app/src/main/java/com/danxx/mdplayer/application/Common.java
@@ -11,4 +11,12 @@ public class Common {
     /**eventbus的显示Action消息类型**/
     public static final int EventBusType_Show = 3;
 
+    /**图片分类**/
+    public static final String img_classify = "http://www.tngou.net/tnfs/api/classify";
+    /**图片列表**/
+    public static final String img_list = "http://www.tngou.net/tnfs/api/list";
+    /**图片详情**/
+    public static final String img_show = "http://www.tngou.net/tnfs/api/show";
+    /**图片URL拼接前缀**/
+    public static final String img_url = "http://tnfs.tngou.net/img";
 }
